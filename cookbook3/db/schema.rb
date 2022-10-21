b020bd45fe782b10685e5995862881942980cfcd
20373630 <20373630@buaa.edu.cn>
2022-10-22 00:25:49 +0800

    ..

--- cookbook3/db/schema.rb
+++ cookbook3/db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_085810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
-    t.string "instructions"
     t.index ["category_id"], name: "index_recipes_on_category_id"
   end
 
