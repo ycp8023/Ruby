3fa3867fb08b3f9214687b50b99040533d8f6111
ycp <20373630@buaa.edu.cn>
2022-10-22 09:40:07 +0800

    ..

--- cookbook3/config/environments/development.rb
+++ cookbook3/config/environments/development.rb
@@ -50,6 +50,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.hosts << "20373630.cookbook3.rubyapp.act.buaa.edu.cn"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
